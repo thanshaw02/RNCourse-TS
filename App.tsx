a33666c8c118aeba145a91ac875e5b9337b9d7b6
Tylor Hanshaw <tylorjhanshaw@xeratec.com>
2023-09-21 16:52:36 -0500

    re-worked 'Styles.ts' to use a new single object that holds all of the specific styles for each component in 'App.ts'

--- App.tsx
+++ App.tsx
@@ -1,14 +1,7 @@
 import { FC, useState } from "react";
 // import { StatusBar } from "expo-status-bar";
 import { Button, Text, TextInput, View } from "react-native";
-
-import {
-  countStyles,
-  globalStyles,
-  normalMessageStyles,
-  secretMessageStyles,
-  viewStyles,
-} from "./Styles";
+import { AppStyles } from "./Styles";
 
 const App: FC<unknown> = () => {
   const [buttonPressed, setButtonPressed] = useState<boolean>(false);
@@ -21,13 +14,13 @@ const App: FC<unknown> = () => {
 
   return (
     // <View /> components act in a similar way to <div />'s where they are containers for some UI
-    <View style={viewStyles}>
-      <Text style={countStyles}>Number of presses: {numberOfPresses}</Text>
+    <View style={AppStyles.container}>
+      <Text style={AppStyles.countStyles}>Number of presses: {numberOfPresses}</Text>
 
       {buttonPressed ? (
-        <Text style={secretMessageStyles}>Super secret message!</Text>
+        <Text style={AppStyles.secretMessageStyles}>Super secret message!</Text>
       ) : (
-        <Text style={normalMessageStyles}>Hello World!</Text>
+        <Text style={AppStyles.normalMessageStyles}>Hello World!</Text>
       )}
 
       <Button title="Click me!" onPress={handleButtonPress} />
--- Styles.ts
+++ Styles.ts
@@ -1,44 +1,43 @@
-import { StyleProp, StyleSheet, TextStyle, ViewStyle } from "react-native";
+import {
+  ImageStyle,
+  StyleProp,
+  StyleSheet,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
 
 /* StyleProp type aliases */
 export type TextStyles = StyleProp<TextStyle>;
 export type ViewStyles = StyleProp<ViewStyle>;
 
-/* Styles that are globally applied */
-const globalStyles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: "#fff",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // },
-});
+type AppStylesType = {
+  container: ViewStyles;
+  countStyles: TextStyles;
+  normalMessageStyles: TextStyles;
+  secretMessageStyles: TextStyles;
+};
 
 /* Styles for specific core components (not global) */
-const viewStyles: ViewStyle = {
-  flex: 1,
-  backgroundColor: "#fff",
-  alignItems: "center",
-  justifyContent: "center",
+const AppStyles: AppStylesType = {
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  countStyles: {
+    marginBottom: 30,
+    paddingBottom: 10,
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  normalMessageStyles: {
+    marginBottom: 15,
+  },
+  secretMessageStyles: {
+    marginBottom: 15,
+    color: "orange",
+  },
 };
 
-const countStyles: TextStyles = {
-  marginBottom: 30,
-};
-
-const normalMessageStyles: TextStyles = {
-  marginBottom: 15,
-};
-
-const secretMessageStyles: TextStyles = {
-  marginBottom: 15,
-  color: "orange",
-};
-
-export {
-  countStyles,
-  globalStyles,
-  normalMessageStyles,
-  secretMessageStyles,
-  viewStyles,
-};
+export { AppStylesType, AppStyles };
