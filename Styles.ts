cdd077a551a0696416c87cd5554958964d3cb88c
Tylor Hanshaw <tylorjhanshaw@xeratec.com>
2023-09-21 17:00:43 -0500

    added in a second un-used styles object with an example of what probably is the prefered way to create styles

--- Styles.ts
+++ Styles.ts
@@ -40,4 +40,27 @@ const AppStyles: AppStylesType = {
   },
 };
 
+/* Looks like this may be the prefered way to set styles, but I like the typed version above better */
+const AppStylesStyleSheet = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  countStyles: {
+    marginBottom: 30,
+    paddingBottom: 10,
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  normalMessageStyles: {
+    marginBottom: 15,
+  },
+  secretMessageStyles: {
+    marginBottom: 15,
+    color: "orange",
+  },
+});
+
 export { AppStylesType, AppStyles };
